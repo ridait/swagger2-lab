ef9ff7ed7e645d6cb3d4f88f9ade52c2aebac028
ridait <rejra2012@gmail.com>
2019-10-12 17:44:04 +0100

    add metadata to swagger documentation

--- swagger2/src/main/java/com/ridait/swagger2/config/SwaggerConfig.java
+++ swagger2/src/main/java/com/ridait/swagger2/config/SwaggerConfig.java
@@ -4,8 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Collections;
 
 @Configuration
 public class SwaggerConfig {
@@ -16,6 +20,20 @@ public class SwaggerConfig {
                 .select()
                 .paths(PathSelectors.ant("/api/**"))
                 .apis(RequestHandlerSelectors.basePackage("com.ridait"))
-                .build();
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo(){
+        return new ApiInfo(
+                "Super Friends API",
+                "sample api to handle friends ( demo swagger features)",
+                "1.0",
+                "Free",
+                new Contact("Rida Ait OUAGHAD","http://ridait.com","contact@ridait.com"),
+                "Licence",
+                "ridait.com",
+                Collections.emptyList()
+        );
     }
 }
